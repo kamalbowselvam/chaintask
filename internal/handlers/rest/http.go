b7b0ed72daf796a78b207a783df092c13867b51a
kamalbowselvam <kselvam.phd@gmail.com>
2023-07-25 16:34:44 +0200

    added mocking

--- internal/handlers/rest/http.go
+++ internal/handlers/rest/http.go
@@ -3,18 +3,13 @@ package rest
 import (
 	"log"
 	"strconv"
-
 	"github.com/gin-gonic/gin"
-	//"github.com/kamalbowselvam/chaintask/internal/core/domain"
 	"github.com/kamalbowselvam/chaintask/internal/core/ports"
 )
-
 
 type HttpHandler struct {
 	taskService ports.TaskService
 }
-
-
 
 func NewHttpHandler(taskService ports.TaskService) *HttpHandler {
 
@@ -23,33 +18,28 @@ func NewHttpHandler(taskService ports.TaskService) *HttpHandler {
 	}
 }
 
+func (h *HttpHandler) GetTask(c *gin.Context) {
 
-
-func(h *HttpHandler) GetTask(c *gin.Context){
-
-	id, err := strconv.ParseInt(c.Param("id"),10,64)
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
 
 	if err != nil {
 		log.Fatal("Could not convert int to string")
 	}
-	task ,err := h.taskService.GetTask(id)
+	task, err := h.taskService.GetTask(id)
 	if err != nil {
-		c.AbortWithStatusJSON(500, gin.H{"message": err.Error()})
+		c.AbortWithStatusJSON(404, gin.H{"message": err.Error()})
 		return
 	}
 	c.JSON(200, task)
 }
 
-
 type TaskParams struct {
-	Name      string    `json:"name"`
-	Budget    float64   `json:"budget"`
-	CreatedBy string    `json:"createdBy"`
-
+	Name      string  `json:"name"`
+	Budget    float64 `json:"budget"`
+	CreatedBy string  `json:"createdBy"`
 }
 
-
-func (h *HttpHandler) CreateTask(c *gin.Context){
+func (h *HttpHandler) CreateTask(c *gin.Context) {
 	taskparam := TaskParams{}
 	c.BindJSON(&taskparam)
 
@@ -62,3 +52,4 @@ func (h *HttpHandler) CreateTask(c *gin.Context){
 
 	c.JSON(200, task)
 }
+
