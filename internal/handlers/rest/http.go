51a959053efc189343747eba02ce816158c56dc9
kamalbowselvam <kselvam.phd@gmail.com>
2023-07-26 12:50:57 +0200

    resolved and rebased with master

--- internal/handlers/rest/http.go
+++ internal/handlers/rest/http.go
@@ -1,16 +1,7 @@
 package rest
 
 import (
-<<<<<<< HEAD
-
-
-=======
-	"log"
 	"strconv"
-<<<<<<< HEAD
->>>>>>> b7b0ed7 (added mocking)
-=======
->>>>>>> b7b0ed72daf796a78b207a783df092c13867b51a
 	"github.com/gin-gonic/gin"
 	"github.com/kamalbowselvam/chaintask/internal/core/ports"
 )
@@ -26,10 +17,7 @@ func NewHttpHandler(taskService ports.TaskService) *HttpHandler {
 	}
 }
 
-func (h *HttpHandler) GetTask(c *gin.Context) {
-<<<<<<< HEAD
 
-<<<<<<< HEAD
 type getTaskRequest struct {
 	Id int64 `uri:"id" binding:"required,min=1"`
 }
@@ -42,26 +30,10 @@ func(h *HttpHandler) GetTask(c *gin.Context){
 	err := c.ShouldBindUri(&req)
 	// id, err := strconv.ParseInt(c.Param("id"),10,64)
 	
-=======
-=======
 
->>>>>>> b7b0ed72daf796a78b207a783df092c13867b51a
 	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
 
->>>>>>> b7b0ed7 (added mocking)
-	if err != nil {
-		c.AbortWithStatusJSON(403, gin.H{"message": err.Error()})
-  return
-	}
-<<<<<<< HEAD
-<<<<<<< HEAD
-	task ,err := h.taskService.GetTask(req.Id)
-=======
 	task, err := h.taskService.GetTask(id)
->>>>>>> b7b0ed7 (added mocking)
-=======
-	task, err := h.taskService.GetTask(id)
->>>>>>> b7b0ed72daf796a78b207a783df092c13867b51a
 	if err != nil {
 		c.AbortWithStatusJSON(404, gin.H{"message": err.Error()})
 		return
