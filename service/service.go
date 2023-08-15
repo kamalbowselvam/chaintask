8a0a1837c081e21ecf4e46d61972d79c56a16b5a
kamalbowselvam <kselvam.phd@gmail.com>
2023-08-15 11:23:04 +0200

    major refactor and added authentication using paseto

--- service/service.go
+++ service/service.go
@@ -10,10 +10,19 @@ import (
 type TaskService interface {
 	GetTask(context.Context, int64) (domain.Task, error)
 	CreateTask(context.Context, db.CreateTaskParams) (domain.Task,error)
+	// DeleteTask(context.Context, int64) error TODO
+	// UpdateTask(context.Contect, db.UpdateTaskParams) (domain.Task,error)
+	
+	
 	CreateUser(context.Context, db.CreateUserParams) (domain.User, error)
 	GetUser(context.Context, string ) (domain.User, error)
-	//SaveTask(domain.Task) (domain.Task, error)
-	//DeleteTask(id int64) error
-	//ListTask(page int64, offset int64) ([]domain.Task, error)
-	//UpdateTask(task domain.Task) (domain.Task, error)
+	// DeleteUser
+	// UpdateUser 
+
+
+	// Create Project 
+	// Get Project 
+	// Delete Project 
+	// Update Project 
+
 }
