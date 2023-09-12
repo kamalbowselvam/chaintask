a00609189b1555ddbf621d71cffbd600b9432d9a
kamalbowselvam <kselvam.phd@gmail.com>
2023-09-12 15:31:01 +0200

    grafana added to the docker file

--- api/project.go
+++ api/project.go
@@ -2,11 +2,11 @@ package api
 
 import (
 	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"github.com/kamalbowselvam/chaintask/db"
 )
-
-
 
 // CreateProject godoc
 // @Summary      Create a project
@@ -15,15 +15,15 @@ import (
 // @Produce      json
 // @Param        request body db.CreateProjectParam true "project creation parameters"
 // @Success      200  {object}  domain.Project
-// @Failure      400  {object}  error 
-// @Failure      404  {object}  error 
-// @Failure      500  {object}  error 
+// @Failure      400  {object}  error
+// @Failure      404  {object}  error
+// @Failure      500  {object}  error
 // @Router       /projects/ [post]
 // @Security BearerAuth
 func (s *Server) CreateProject(c *gin.Context){
 	
 	projectparam := db.CreateProjectParam{}
-	c.BindJSON(&projectparam)
+	c.ShouldBindBodyWith(&projectparam, binding.JSON)
 	log.Println(projectparam)
 
 	task, err := s.service.CreateProject(c, projectparam)
