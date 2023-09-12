76212983e9e24db2477061da888218a108cf6d55
Ménélik Vero <menelik.vero@tum.de>
2023-09-12 18:43:45 +0000

    add minimal working and simpler test

---
+++ authorization/casbin_authorization_test.go
@@ -0,0 +1,32 @@
+package authorization
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/casbin/casbin/v2"
+	"github.com/casbin/casbin/v2/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEnforcing(t *testing.T) {
+
+	policies := `p, kselvamADMIN, /users*, GET|POST|DELETE|PUT
+p, kselvamADMIN, /projects*, GET|POST|DELETE|PUT
+p, toto1,       /projects/1*, GET|POST|DELETE|PUT
+`
+	file, err := ioutil.TempFile("", "myname.*.bat")
+	defer os.RemoveAll(file.Name())
+	file.WriteString(policies)
+	require.NoError(t, err)
+	enforcer, err := casbin.NewEnforcer("../config/rbac_model.conf", file.Name())
+	require.NoError(t, err)
+	require.NotEmpty(t, enforcer)
+	enforcer.AddNamedMatchingFunc("g", "KeyMatch2", util.KeyMatch)
+	enforcer.AddNamedMatchingFunc("g", "KeyMatch2", util.RegexMatch)
+	enforcer.EnableLog(true)
+	cond, err := enforcer.Enforce("kselvamADMIN", "/projects/1", "GET")
+	require.NoError(t, err)
+	require.True(t, cond)
+}
