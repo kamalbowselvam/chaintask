a2c0823f5ff44d45579a5f02494fbf9b61d09a1d
Ménélik Vero <menelik.vero@tum.de>
2023-11-18 17:37:42 +0000

    update

--- db/project.go
+++ db/project.go
@@ -13,11 +13,12 @@ const createProject = `INSERT INTO projects (
 	location,
 	address,
 	responsible,
-	client
+	client,
+	company_id
   ) VALUES (
 	$1, $2, $3, $4, $5, $6
   )
-  RETURNING id, projectname, created_at, created_by, location, address, responsible, client;`
+  RETURNING id, projectname, created_at, created_by, location, address, responsible, client, company_id;`
 
 type CreateProjectParam struct {
 	ProjectName string          `json:"projectname" `
@@ -26,12 +27,13 @@ type CreateProjectParam struct {
 	Address     string          `json:"address"`
 	Responsible string          `json:"responsible"`
 	Client      string          `json:"client"`
+	CompanyId   int64           `json:"company_id"`
 }
 
 func (q *Queries) CreateProject(ctx context.Context, arg CreateProjectParam) (domain.Project, error) {
 	logger.Info("saving projects")
 
-	row := q.db.QueryRowContext(ctx, createProject, arg.ProjectName, arg.CreatedBy, Point{arg.Location[0], arg.Location[1]}, arg.Address, arg.Responsible, arg.Client)
+	row := q.db.QueryRowContext(ctx, createProject, arg.ProjectName, arg.CreatedBy, Point{arg.Location[0], arg.Location[1]}, arg.Address, arg.Responsible, arg.Client, arg.CompanyId)
 	var i domain.Project
 	var p Point
 	err := row.Scan(
@@ -43,6 +45,7 @@ func (q *Queries) CreateProject(ctx context.Context, arg CreateProjectParam) (do
 		&i.Address,
 		&i.Responsible,
 		&i.Client,
+		&i.CompanyId,
 	)
 	i.Location = domain.Location{p[0], p[1]}
 	return i, err
@@ -68,7 +71,7 @@ func (q *Queries) DeleteProject(ctx context.Context, projectId int64) (error){
 	return err
 }
 
-const getProject = `select projectname, created_at, created_by, location, address, responsible, client FROM projects where id = $1`
+const getProject = `select projectname, created_at, created_by, location, address, responsible, client, company_id FROM projects where id = $1`
 func (q *Queries) GetProject(ctx context.Context, projectId int64) (domain.Project, error){
 	row := q.db.QueryRowContext(ctx, getProject, projectId)
 	var i domain.Project
@@ -82,6 +85,7 @@ func (q *Queries) GetProject(ctx context.Context, projectId int64) (domain.Proje
 		&i.Address,
 		&i.Responsible,
 		&i.Client,
+		&i.CompanyId,
 	)
 	i.Location = domain.Location{p[0], p[1]}
 	return i, err
--- domain/project.go
+++ domain/project.go
@@ -9,8 +9,8 @@ type Location [2]float64
 type Project struct {
 	Id                   int64     `json:"id"`
 	Projectname          string    `json:"projectname"`
-	CreatedOn            time.Time `json:"createdOn"`
-	CreatedBy            string    `json:"createdBy"`
+	CreatedOn            time.Time `json:"created_on"`
+	CreatedBy            string    `json:"created_by"`
 	Location             Location  `json:"location"`
 	Address              string    `json:"address"`
 	Responsible          string    `json:"responsible"`
@@ -18,6 +18,7 @@ type Project struct {
 	Tasks                []Task    `json:"tasks"`
 	CompletionPercentage float64   `json:"completion_percentage"`
 	Budget               float64   `json:"budget"`
+	CompanyId            int64     `json:"company_id"`
 }
 
 func NewProject(projectname string, address string, location Location, responsible string, client string, user string) Project {
